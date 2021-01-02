e21131f03e8f28db715dd4e20b6c2a3d62fdde2d
Vladimir Grichina <vgrichina@componentix.com>
2021-01-01 23:50:10 -0800

    Use PG connection pool

--- indexer.js
+++ indexer.js
@@ -1,35 +1,41 @@
-const { Client } = require('pg');
+const { Pool } = require('pg');
 
 const INDEXER_DB_CONNECTION = process.env.INDEXER_DB_CONNECTION;
 
-let client;
-async function getPgClient() {
-    if (!client) {
-        client = new Client({
+let pool;
+async function withPgClient(fn) {
+    if (!pool) {
+        pool = new Pool({
             connectionString: INDEXER_DB_CONNECTION,
         });
-        await client.connect();
     }
+    const client = await pool.connect();
 
-    return client;
+    try {
+        return fn(client);
+    } finally {
+        client.release();
+    }
 }
+
+
 
 async function findEdits(accountId) {
     console.log('findEdits', accountId);
 
-    const client = await getPgClient();
-    const { rows } = await client.query(`
-        SELECT included_in_block_timestamp AS block_timestamp, included_in_block_hash AS block_hash
-        FROM receipts ${accountId ? '' : `TABLESAMPLE SYSTEM(0.05)`}
-        WHERE receiver_account_id = 'berryclub.ek.near'
-            ${accountId ? `AND predecessor_account_id = $1` : ''}
-        ORDER BY random()
-        LIMIT 50
-        `, accountId ? [accountId] : []);
+    return withPgClient(async client => {
+        const { rows } = await client.query(`
+            SELECT included_in_block_timestamp AS block_timestamp, included_in_block_hash AS block_hash
+            FROM receipts ${accountId ? '' : `TABLESAMPLE SYSTEM(0.05)`}
+            WHERE receiver_account_id = 'berryclub.ek.near'
+                ${accountId ? `AND predecessor_account_id = $1` : ''}
+            ORDER BY random()
+            LIMIT 50
+            `, accountId ? [accountId] : []);
 
-    console.log(`Found ${rows.length} rows`);
-
-    return rows;
+        console.log(`Found ${rows.length} rows`);
+        return rows;
+    })
 }
 
 module.exports = { findEdits };
