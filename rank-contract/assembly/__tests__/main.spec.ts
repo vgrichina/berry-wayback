78a3c87e1c2a93e3010dd607747d590b75de1e0a
Vladimir Grichina <vgrichina@componentix.com>
2021-01-17 22:59:17 -0800

    Implement updating ELO rankings

--- rank-contract/assembly/__tests__/main.spec.ts
+++ rank-contract/assembly/__tests__/main.spec.ts
@@ -1,9 +1,17 @@
-import { setGreeting } from '..';
+import { vote, getEloRating } from '..';
 import { storage, Context } from "near-sdk-as";
 
-describe("Greeting ", () => {
-    it("should be set and read", () => {
-        setGreeting("hello world");
-        const greeting = storage.get<string>(Context.sender);
+describe("Ranking", () => {
+    it("can be updated by vote", () => {
+        storage.set("elo:a", "140000");
+        storage.set("elo:b", "110000");
+
+        const scores = vote([
+            { id: "a", score: 0 },
+            { id: "b", score: 100 },
+        ])
+        
+        expect(scores[0]).toBe(137283);
+        expect(scores[1]).toBe(112716);
     });
 });
